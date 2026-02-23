2e2cdb803358b016c059a4a60a285dc9e0c8150d
Mathieu-Bailliez <12996@holbertonstudents.cm>
2026-02-23 17:28:43 +0100

    prints the size of various types on the computer

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("Taille d'un char : %zu byte(s)\n", sizeof(char));
-	printf("Taille d'un int : %zu byte(s)\n", sizeof(int));
-	printf("Taille d'un long int : %zu byte(s)\n", sizeof(long int));
-	printf("Taille d'un long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Taille d'un float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char : %zu byte(s)\n", sizeof(char));
+	printf("Size of a int : %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int : %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
