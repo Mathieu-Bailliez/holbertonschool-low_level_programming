e4b7eb3f30596def5412f893ef796daa4c68c8ae
Mathieu-Bailliez <12996@holbertonstudents.cm>
2026-02-25 13:50:45 +0100

    Print 0-9

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-        int c = 0;
+	int c = 0;
 
-        while (c < 10)
-        {
-                putchar(c + '0');
-                c++;
-        }
-        putchar('\n');
+	while (c < 10)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
