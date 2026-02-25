11ee2dbbc4715cd9ac0169f80cba636d6d23ad66
Mathieu-Bailliez <12996@holbertonstudents.cm>
2026-02-25 13:10:53 +0100

    prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			putchar (c);
+			c++;
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}
