01be4052a6df2b538740e3a97c5a05cf2624ca86
Mathieu-Bailliez <12996@holbertonstudents.cm>
2026-02-28 16:42:15 +0100

    Checks for uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if uppercase , 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
