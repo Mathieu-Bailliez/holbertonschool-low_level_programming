efe031ce39f45474f07bb0be6c4cd3d97abe17f6
Mathieu-Bailliez <12996@holbertonstudents.cm>
2026-03-01 21:14:50 +0100

    Print all minutes and hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,24 +5,24 @@
  */
 void jack_bauer(void)
 {
-    int hour;
-    int minute;
+	int hour;
+	int minute;
 
-    hour = 0;
-    while (hour < 24)
-    {
-        minute = 0;
-        while (minute < 60)
-        {
-            _putchar((hour / 10) + '0');
-            _putchar((hour % 10) + '0');
-            _putchar(':');
-            _putchar((minute / 10) + '0');
-            _putchar((minute % 10) + '0');
-            _putchar('\n');
+	hour = 0;
+	while (hour < 24)
+	{
+		minute = 0;
+		while (minute < 60)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 
-            minute++;
-        }
-        hour++;
-    }
+			minute++;
+		}
+		hour++;
+	}
 }
