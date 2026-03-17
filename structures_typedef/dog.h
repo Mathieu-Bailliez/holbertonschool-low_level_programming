f4413ad9279bc89e459715101c2887e864162c95
Mathieu-Bailliez <12996@holbertonstudents.cm>
2026-03-17 21:11:14 +0100

    Fix add Dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog
+/**
+ * struct dog - Structure représentant un chien
+ * @name: Le nom du chien
+ * @age: L'âge du chien
+ * @owner: Le propriétaire du chien
+ */
+struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+	char  *name;
+	float  age;
+	char  *owner;
+};
 
-#endif
+/**
+ * typedef struct dog dog_t - Alias pour struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
