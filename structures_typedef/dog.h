3ae1120fa2c926ce3e13e85f78eeaa1f145df315
Mathieu-Bailliez <12996@holbertonstudents.cm>
2026-03-18 01:43:32 +0100

    Define a new type dog_t for type the struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,19 +2,18 @@
 #define DOG_H
 
 /**
-* struct dog - define a dog object
-* @name: string - used for the dog's name
-* @age: integer - used for dog's age
-* @owner: string - used for the dog's owner name
-*/
-typedef struct dog
+ * struct dog - Structure de base pour un chien
+ * @name: Nom du chien (chaîne de caractères)
+ * @age: Âge du chien (nombre flottant)
+ * @owner: Nom du propriétaire (chaîne de caractères)
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+typedef struct dog dog_t;
+
 #endif
