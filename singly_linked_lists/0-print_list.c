24a8a6215176521bf7bbd0e2b685b8e6f591782f
Mathieu-Bailliez <12996@holbertonstudents.cm>
2026-03-26 17:54:15 +0100

    Write a function that prints all the elements of a list_t list.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - affiche tous les éléments d'une liste chaînée
+ * @h: pointeur vers le premier noeud
+ *
+ * Return: nombre de noeuds
+ */
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+        {
+            printf("[0] (nil)\n");
+        }
+        else
+        {
+            printf("[%u] %s\n", h->len, h->str);
+        }
+
+        h = h->next;
+        count++;
+    }
+
+    return (count);
+}
