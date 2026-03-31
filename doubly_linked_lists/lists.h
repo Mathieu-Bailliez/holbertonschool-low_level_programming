a33bb8eb15b0abc01f23a999bb4a93c0d3392c10
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-03-31 18:02:56 +0200

    insert node at specific index in doubly linked list

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -11,7 +11,6 @@
  *
  * Description: structure for doubly linked list
  */
-
 typedef struct dlistint_s
 {
 	int n;
@@ -26,6 +25,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
-
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif
