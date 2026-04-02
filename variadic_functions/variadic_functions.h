8f59daf251ea4dd41f15fd116ba1a42fd8a872e4
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-04-02 07:30:27 +0200

    write a function that returns the sum of all its parameters

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all parameters
+ * @n: number of arguments
+ *
+ * Return: sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
