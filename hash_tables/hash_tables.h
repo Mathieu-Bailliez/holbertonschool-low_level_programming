6ed1855968539bc9c001385538090df936ab45b7
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-04-02 07:04:41 +0200

    write a function that deletes a hash table

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -33,5 +33,6 @@ unsigned long int key_index(const unsigned char *key,
 	unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key,
 	const char *value);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
