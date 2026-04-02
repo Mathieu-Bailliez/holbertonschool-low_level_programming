6c7c1ad132a4bd579f3a6c271f7d95f8d5a798bf
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-04-02 07:38:31 +0200

    fix print_all to respect if constraints

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,42 +2,84 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - prints a char
+ * @args: arguments
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an int
+ * @args: arguments
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: arguments
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string
+ * @args: arguments
+ */
+void print_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
  * print_all - prints anything
  * @format: list of types
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
+	unsigned int i = 0, j;
 	char *sep = "";
-	char *str;
+
+	struct printer
+	{
+		char *type;
+		void (*f)(va_list);
+	};
+
+	struct printer p[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		j = 0;
+		while (p[j].type)
 		{
-			printf("%s%c", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		if (format[i] == 'i')
-		{
-			printf("%s%d", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		if (format[i] == 'f')
-		{
-			printf("%s%f", sep, va_arg(args, double));
-			sep = ", ";
-		}
-		if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", sep, str);
-			sep = ", ";
+			if (*p[j].type == format[i])
+			{
+				printf("%s", sep);
+				p[j].f(args);
+				sep = ", ";
+			}
+			j++;
 		}
 		i++;
 	}
