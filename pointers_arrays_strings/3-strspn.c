0f7c9272da53890c6fb70cc8a14a565db8729038
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-04-24 13:07:28 +0200

    Task 3: implement _strspn function

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be scanned
+ * @accept: string containing accepted bytes
+ *
+ * Return: number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	int found;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		found = 0;
+		j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+			j++;
+		}
+
+		if (found == 0)
+			break;
+
+		i++;
+	}
+
+	return (i);
+}
