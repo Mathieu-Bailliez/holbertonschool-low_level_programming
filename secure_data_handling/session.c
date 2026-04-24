0f7aec5dd1fc211047c77be2e0b9b343fbf6d066
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-04-24 13:55:42 +0200

    Fix session ownership, safe allocation, and update handling

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -2,67 +2,92 @@
 #include <string.h>
 #include "session.h"
 
-session_t *session_create(const char *id, unsigned int uid, const unsigned char *data, size_t data_len)
+/**
+ * session_create - create a new session
+ */
+session_t *session_create(const char *id, unsigned int uid,
+	const unsigned char *data, size_t data_len)
 {
 	session_t *s;
 
-	s = (session_t *)malloc(sizeof(*s));
+	s = malloc(sizeof(*s));
 	if (!s)
-		return NULL;
+		return (NULL);
 
-	s->id = (char *)id;
+	/* Deep copy of id */
+	s->id = strdup(id);
+	if (!s->id)
+	{
+		free(s);
+		return (NULL);
+	}
 
 	s->uid = uid;
 
-	if (data_len > 0) {
-		s->data = (unsigned char *)malloc(data_len);
-		if (!s->data) {
-			return NULL;
+	/* Copy data if exists */
+	if (data_len > 0)
+	{
+		s->data = malloc(data_len);
+		if (!s->data)
+		{
+			free(s->id);
+			free(s);
+			return (NULL);
 		}
 		memcpy(s->data, data, data_len);
 		s->data_len = data_len;
-	} else {
+	}
+	else
+	{
 		s->data = NULL;
 		s->data_len = 0;
 	}
 
-	return s;
+	return (s);
 }
 
-int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
+/**
+ * session_set_data - update session data safely
+ */
+int session_set_data(session_t *s, const unsigned char *data,
+	size_t data_len)
 {
 	unsigned char *tmp;
 
 	if (!s)
-		return 0;
+		return (0);
 
-	if (data_len == 0) {
+	/* Clear data */
+	if (data_len == 0)
+	{
 		free(s->data);
 		s->data = NULL;
 		s->data_len = 0;
-		return 1;
+		return (1);
 	}
 
-	tmp = (unsigned char *)realloc(s->data, data_len);
+	/* Safe realloc */
+	tmp = realloc(s->data, data_len);
+	if (!tmp)
+		return (0);
+
 	s->data = tmp;
-
-	if (!s->data) {
-		s->data_len = 0;
-		return 0;
-	}
 
 	memcpy(s->data, data, data_len);
 	s->data_len = data_len;
-	return 1;
+
+	return (1);
 }
 
+/**
+ * session_destroy - free session safely
+ */
 void session_destroy(session_t *s)
 {
 	if (!s)
 		return;
 
 	free(s->id);
-
 	free(s->data);
 	free(s);
 }
